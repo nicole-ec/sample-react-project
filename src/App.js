8c194e95a77e078aa9553efea9116b60a54a06ba
nicole-ec <nicole.caballero@ucalgary.ca>
2023-04-17 17:35:46 -0600

    react sample project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+// import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchString: ''
+    }; 
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=> response.json() )
+    .then((users)=> this.setState(() => {
+        return {monsters: users}
+    }))
+  }
+
+  onSearchChange = (event) => {
+    const searchString = event.target.value.toLowerCase();
+            
+    this.setState(() => {
+      return {searchString}
+    });
+    // return this.state.monsters.filter(monster => monster.name.includes(event.target.value)); ---> cant return this, general practice is to use non modifying methods (if ur changing array just make a new one)
+  }
+
+  render() {
+
+    const {monsters, searchString} = this.state;//initializing these variables
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchString));
+
+    return (
+      <div className='App'>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox 
+          className="search-box" 
+          onChangeHandler={onSearchChange} 
+          placeholder="search monsters"
+        />
+        <CardList monsters={filteredMonsters}/>
+        {/* {
+          filteredMonsters.map((monster)=> {
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
+          })
+        } */}
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Hi {this.state.name.first} {this.state.name.last}, I work at {this.state.company}
+      //     </p>
+      //     <button onClick={() => {
+      //       // for synchronous call, ideal way to write code: 
+      //       this.setState(() => {
+      //         return {
+      //           name: {first: "Andrei", last:"Neaogie"}
+      //         };
+      //       }, ()=>{//callback (optional), runs after the function is done
+      //         console.log(this.state);
+      //       });
+            
+      //       //for asynchronous call:
+      //       // this.setState({name: {first: "Andrei", last:"Neaogie"}});
+      //     }}>
+      //       Change Name
+      //     </button>
+      //   </header>
+      // </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import "./card-list.styles.css";
+import Card from "../card/card.component";
+
+class CardList extends Component {
+    
+    render() {
+        const {monsters} = this.props; 
+        return (
+            <div className="card-list">
+                {
+                    monsters.map((monster)=> {
+                        return(
+                         <Card monster={monster}/>   
+                        );
+                        // return <div key={monster.id}><h1>{monster.name}</h1></div>
+                    })
+                }
+            </div> 
+        );
+    } 
+}
+
+export default CardList;
